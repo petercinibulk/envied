d80f8e575b4ad7d08a3724fea6be7bd480f9a83f
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-10-07 20:43:48 +0300

    Add several test for defaul decorator added

--- packages/envied/test/envy_test.dart
+++ packages/envied/test/envy_test.dart
@@ -48,4 +48,32 @@ void main() {
       expect(enviedField.obfuscate, true);
     });
   });
+
+  group('Default Decorator Test Group', () {
+    test('Empty constructor', () {
+      final d = Default(null);
+      expect(d.defaultValue, null);
+    });
+
+    test('Specified value as bool', () {
+      final d = Default(true);
+
+      expect(d.defaultValue is bool, true);
+      expect(d.defaultValue, true);
+    });
+
+    test('Specified obfuscate as String', () {
+      final d = Default('true');
+
+      expect(d.defaultValue is String, true);
+      expect(d.defaultValue, 'true');
+    });
+
+    test('Specified obfuscate as int', () {
+      final d = Default(0);
+
+      expect(d.defaultValue is int, true);
+      expect(d.defaultValue, 0);
+    });
+  });
 }
