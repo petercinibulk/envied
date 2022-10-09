d7c049850796300198417506eb69997f108fe759
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-10-09 13:56:32 +0300

    Update Default decorator added to example

--- examples/envied_example/lib/debug_env.g.dart
+++ examples/envied_example/lib/debug_env.g.dart
@@ -9,4 +9,7 @@ part of 'debug_env.dart';
 class _Env {
   static const String key1 = 'DEBUG1';
   static const String key2 = 'DEBUG2';
+  static const String key3 = 'test_';
+  static const int key4 = 0;
+  static const bool key5 = true;
 }
--- examples/envied_example/lib/release_env.g.dart
+++ examples/envied_example/lib/release_env.g.dart
@@ -9,4 +9,7 @@ part of 'release_env.dart';
 class _Env {
   static const String key1 = 'VALUE1';
   static const String key2 = 'VALUE2';
+  static const String key3 = 'test_';
+  static const int key4 = 0;
+  static const bool key5 = true;
 }
