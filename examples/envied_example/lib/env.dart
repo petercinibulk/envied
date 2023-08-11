7110cc7b9155048916d8aa34e2bbbb6207e1381e
Klemen Tušar <techouse@gmail.com>
2023-08-11 08:01:36 +0200

    :technologist: fix/upgrade example

--- examples/envied_example/lib/env.dart
+++ examples/envied_example/lib/env.dart
@@ -6,9 +6,9 @@ part 'env.g.dart';
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'KEY1')
-  static const key1 = _Env.key1;
+  static const String key1 = _Env.key1;
   @EnviedField(varName: 'KEY2')
-  static const key2 = _Env.key2;
+  static const String key2 = _Env.key2;
   @EnviedField()
   static const String key3 = _Env.key3;
   @EnviedField()
