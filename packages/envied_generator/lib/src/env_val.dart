e21f3dc4984889f9462de908f49d073ea3937792
Klemen Tušar <techouse@gmail.com>
2024-03-09 14:02:56 +0000

    :bulb: add EnvVal description

--- packages/envied_generator/lib/src/env_val.dart
+++ packages/envied_generator/lib/src/env_val.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 
+/// Represents a raw environment variable as well as its interpolated value.
 class EnvVal with EquatableMixin {
   const EnvVal({
     required this.raw,
